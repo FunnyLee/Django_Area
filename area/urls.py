28c703959d95d26009bca3616a5454720f677104
FunnyLee <13066807136@163.com>
2019-07-10 23:30:03 +0800

    实现省市选择

--- area/urls.py
+++ area/urls.py
@@ -4,4 +4,7 @@ from area import views
 
 urlpatterns = [
     url(r'^show_area$', views.show_area, name='show_area'),  # 显示地区详情
+    url(r'^show_select_area$', views.show_select_area, name='show_select_area'),  # 显示省市县选择页面
+    url(r'^get_prov$', views.get_prov, name='get_prov'),  # 获取省级地区
+    url(r'^get_city$', views.get_city, name='get_city'),  # 获取市级地区
 ]
--- area/views.py
+++ area/views.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,6 +7,7 @@ from django.shortcuts import render
 from area.models import AreaInfo
 
 
+# show_area
 def show_area(request):
     '''显示地区详情'''
     area = AreaInfo.objects.get(atitle='广州市')
@@ -14,3 +16,42 @@ def show_area(request):
 
     child_area = AreaInfo.objects.filter(aparent__exact=area)
     return render(request, 'area/show_area.html', {'area': area, 'child_area': child_area})
+
+
+# /show_select_area
+def show_select_area(request):
+    '''显示省市县选择页面'''
+    return render(request, 'area/select_area.html')
+
+
+# /get_prov
+def get_prov(request):
+    '''获取省级数据'''
+    # aparent为空，表示省
+    provs = AreaInfo.objects.filter(aparent__isnull=True)
+
+    if provs:
+        prov_list = list()
+        for prov in provs:
+            tuple = (prov.id, prov.atitle)
+            prov_list.append(tuple)
+
+    return JsonResponse({'prov_list': prov_list})
+
+
+# /get_city
+def get_city(request):
+    '''获取市级数据'''
+
+    param_dict = request.GET
+    prov_id = param_dict.get("prov_id");
+
+    citys = AreaInfo.objects.filter(aparent_id__exact=prov_id)
+
+    if citys:
+        city_list = list()
+        for city in citys:
+            tuple = (city.id, city.atitle)
+            city_list.append(tuple)
+    print(city_list)
+    return JsonResponse({'city_list': city_list})
